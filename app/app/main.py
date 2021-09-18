d315f01e3aa02f24c712a22f920210da6c281aad
Jimmy Yue <53287360+jimmyue@users.noreply.github.com>
2021-09-18 16:49:25 +0800

    Add files via upload

---
+++ app/app/main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+'''
+Created on 2021年9月16日
+@author: yuejing
+'''
+import pymysql
+from flask import Flask
+from flask_restful import reqparse, Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+def get_dealer(dealer_code):
+    conn = pymysql.connect(host='10.10.22.172',port=3306,user='root',passwd='abc.1234',db='ways_lexus',use_unicode=True, charset="utf8")
+    cur = conn.cursor()
+    sql = "select DEALER_CODE,DEALER_NAME from tb_dealer_info where DEALER_CODE='%s'" % dealer_code
+    cur.execute(sql)
+    data = cur.fetchone()
+    if data==None:
+        result = {'status':204,'message':'经销店不存在','data':{'DEALER_CODE':'null','DEALER_NAME':'null'}}
+    else:
+        result = {'status':200,'message':'获取成功','data':{'DEALER_CODE':data[0],'DEALER_NAME':data[1]}}
+    return result
+
+class dealer(Resource):
+        def __init__(self):
+                self.parser = reqparse.RequestParser()
+                self.parser.add_argument('dealer_code', type=str, required=True, help='dealer_code is a required field')
+                self.args = self.parser.parse_args()
+
+        def get(self):
+                get_data=get_dealer(self.args.dealer_code)
+                return get_data
+
+        def post(self):
+                post_data=get_dealer(self.args.dealer_code)
+                return post_data
+
+api.add_resource(dealer, '/dealer')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=80)
